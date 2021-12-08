b755588ac50e4b2dfb8ae58101039e82927980b8
Иван Питенин <ivan.pitenin@gmail.com>
2021-12-08 23:14:37 +0300

    внес правки после комментариев

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import {renderTemplate, RenderPosition} from './render.js';
-import {createButtonTemplate} from './view/show-more-button.js';
+import {createShowMoreButtonTemplate} from './view/show-more-button.js';
 import {createCardTemplate} from './view/card.js';
-import {createFilmsWrapperTemplate} from './view/films-wrapper';
+import {createFilmsWrapperTemplate} from './view/films-wrapper.js';
 import {createMenuTemplate} from './view/menu.js';
 import {createPopupTemplate} from './view/popup.js';
 import {createUserStatusTemplate} from './view/user-status.js';
@@ -9,6 +9,8 @@ import {createUserStatusTemplate} from './view/user-status.js';
 const pageHeader = document.querySelector('.header');
 const pageMain = document.querySelector('.main');
 const pageFooter = document.querySelector('.footer');
+const FILM_LIST_CARDS = 5;
+const FILM_LIST_CARDS_SUGGESTION = 2;
 
 renderTemplate(pageHeader, createUserStatusTemplate(), RenderPosition.BEFOREEND);
 renderTemplate(pageMain, createMenuTemplate(), RenderPosition.AFTERBEGIN);
@@ -16,19 +18,15 @@ renderTemplate(pageMain, createFilmsWrapperTemplate(), RenderPosition.BEFOREEND)
 
 const filmsList = pageMain.querySelector('.films-list__container');
 const filmsListSuggestion = pageMain.querySelectorAll('.films-list--extra .films-list__container');
-const FILM_LIST_CARDS = 5;
-const FILM_LIST_CARDS_SUGGESTION = 2;
 
 for (let i = 0; i < FILM_LIST_CARDS; i++) {
   renderTemplate(filmsList, createCardTemplate(), RenderPosition.AFTERBEGIN);
 }
-renderTemplate(filmsList, createButtonTemplate(), RenderPosition.AFTEREND);
+renderTemplate(filmsList, createShowMoreButtonTemplate(), RenderPosition.AFTEREND);
 filmsListSuggestion.forEach((filmsListSuggestionItem) => {
-  for(let i = 0; i < FILM_LIST_CARDS_SUGGESTION; i++) {
+  for (let i = 0; i < FILM_LIST_CARDS_SUGGESTION; i++) {
     renderTemplate(filmsListSuggestionItem, createCardTemplate(), RenderPosition.BEFOREEND);
   }
 });
 
 renderTemplate(pageFooter, createPopupTemplate(), RenderPosition.AFTEREND);
-const popup = document.querySelector('.film-details');
-popup.style.display = 'none';
--- src/view/show-more-button.js
+++ src/view/show-more-button.js
@@ -1,4 +1,4 @@
-export const createButtonTemplate = () => (
+export const createShowMoreButtonTemplate = () => (
   `<button class="films-list__show-more">
   Show more</button>`
 );
